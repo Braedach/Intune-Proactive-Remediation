221f3299a752f5eb820a60b02d1c79ee31558b67
Braedach <40484379+Braedach@users.noreply.github.com>
2021-07-08 10:09:51 +0800

    Add files via upload
    Initial Upload
    Holding code deployment on 24 hour lag in M365 Security portal
    All PC's are at version 10.0.x.1083 so OOB for July has deployed

---
+++ Microsoft KB5005010 Detection.ps1
@@ -0,0 +1,37 @@
+﻿<#
+   Code sourced from here:  https://thecollective.eu/blog/implement-workarounds-for-pinter-nightmare-with-mem/
+   Code copyright of THIJS LECOMTE
+
+   Code modified from here :  https://support.microsoft.com/en-us/topic/kb5005010-restricting-installation-of-new-printer-drivers-after-applying-the-july-6-2021-updates-31b91c02-05bc-4ada-a7ea-183b129578a7
+
+   Detection script for Printnightmare KB5005010
+
+   Code not used but under investigation.
+
+#>
+
+
+
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint"
+$RegKey = "RestrictDriverInstallationToAdministrators"
+$RegValue = 1
+
+try{
+    if(!(Test-Path $RegPath -ErrorAction Stop)){
+        Write-Host "Path doesn't exist"
+        Exit 1
+    }
+    $key = Get-ItemProperty -Path $RegPath | Select-Object -Property $RegKey -ErrorAction Stop
+    if($key."$RegKey" -eq $RegValue){
+        Write-Host "Key has correct value" 
+        Exit 0
+    }
+    else{
+        Write-Host "Key has incorrect value or doesn't exist"
+        Exit 1
+    }
+}
+catch{
+    Write-Host "Key doesn't exist"
+    Exit 1
+}
---
+++ Microsoft KB5005010 Remediation.ps1
@@ -0,0 +1,34 @@
+﻿<#
+   Code sourced from here:  https://thecollective.eu/blog/implement-workarounds-for-pinter-nightmare-with-mem/
+   Code copyright of THIJS LECOMTE
+
+   Code modified from here :  https://support.microsoft.com/en-us/topic/kb5005010-restricting-installation-of-new-printer-drivers-after-applying-the-july-6-2021-updates-31b91c02-05bc-4ada-a7ea-183b129578a7
+
+   Remediation script for Printnightmare KB5005010
+
+
+   Code not used but under investigation
+
+#>
+
+
+
+
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint"
+$RegKey = "RestrictDriverInstallationToAdministrators"
+$RegValue = 1
+
+if(!(Test-Path $RegPath -ErrorAction Stop)){
+    New-Item $RegPath
+    Write-Host "Created path"
+}
+try{
+    Set-ItemProperty -Path $RegPath -Name $RegKey -Value $RegValue
+    Write-Host "Key has been set"
+
+    Restart-Service -Name "Spooler" -force
+    Write-Host "Spooler has been reset"
+}
+catch{
+    Write-Error "Error setting key"
+}
